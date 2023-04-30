86f00d17fe4f028faf193c634cf0ac14c4bf1edb
Sanderson Nyange <sandersonnyange@gmail.com>
2023-04-29 19:16:06 -0700

    Few tweaks

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -7,7 +7,7 @@
  */
 void shell_sort(int *array, size_t size)
 {
-		size_t n, gap = 1, j;
+		size_t i, gap = 1, j;
 		int tmp, tmpint;
 
 		if (size < 2)
@@ -19,10 +19,10 @@ void shell_sort(int *array, size_t size)
 		}
 		for (; gap >= 1; gap /= 3) /*Decreases gap from max value to 1*/
 		{
-			for (n = gap; n < size; i++)
+			for (i = gap; i < size; i++)
 			{
 				tmp = array[n];
-				for (j = n; j >= gap && array[j - gap] > tmp; j -= gap)
+				for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
 				{
 					tmpint = array[j];
 					array[j] = array[j - gap];
